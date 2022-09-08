9411807fa60b4b89b17aa31ea5773ce59e6607c4
godric4 <gmarto400@gmail.com>
2022-09-08 16:33:52 -0700

    betty me

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
-#include <stdio.h> 
-int main(void) {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+#include <stdio.h>
+/**
+ *main - this is the entry poINT
+ * BETTY LEAVE ME ALONE
+ * Return: 0 after the function completes
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
