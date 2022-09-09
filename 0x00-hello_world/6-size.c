92370956deca62a3cd127e487ceada1637085ced
godric4 <gmarto400@gmail.com>
2022-09-08 17:42:22 -0700

    hhh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,12 +10,12 @@ int a;
 long int b;
 long long int c;
 char d;
-float;
+float f;
 
 print("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 print("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));  
 print("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));  
 print("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));  
 print("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));  
-return (0)
+return (0);
 }
