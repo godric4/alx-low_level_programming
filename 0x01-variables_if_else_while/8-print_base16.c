ec6a7032756fa2c598e025804fa5af2b0637207f
godric4 <gmarto400@gmail.com>
2022-09-09 16:06:10 -0700

    he

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,10 +12,10 @@ for (hexint = 48; hexint < 58; hexint++)
 {
 putchar(hexint);
 }
-for (hex = 'a'; hex <= 'f'; hex++)
+/*for (hex = 'a'; hex <= 'f'; hex++)
 {
 putchar(hex);
-}
+}*/
 putchar('\n');
 
 return (0);
