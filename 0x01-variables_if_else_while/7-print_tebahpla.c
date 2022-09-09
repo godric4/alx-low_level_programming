6173bf7ea87151202560617a7eee955e931530db
godric4 <gmarto400@gmail.com>
2022-09-09 14:58:13 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * display
+ * Return: 0 success
+ */
+int main(void)
+{
+char a;
+for (a = 122; a >= 97; alphabet--)
+{
+putchar(a);
+}
+putchar('\n')
+return (0)
+}
