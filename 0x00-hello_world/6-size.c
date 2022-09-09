7a4fe894270324330c67b974ad7be183f0e1c78f
godric4 <gmarto400@gmail.com>
2022-09-08 17:36:40 -0700

    hhh

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * betty me
+ * Retun: 0 when function completes
+ */
+int main(void)
+{
+int a;
+long int b;
+long long int c;
+char d;
+float;
+
+print("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+print("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));  
+print("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));  
+print("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));  
+print("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));  
+return (0)
+}
