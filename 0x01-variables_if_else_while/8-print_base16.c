9fe598bf85de16d2054f6155208a5ed085955790
godric4 <gmarto400@gmail.com>
2022-09-09 15:34:05 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *  * main - entry point
+ *   * display numbers
+ *    * Return: 0 success
+ *     */
+int main(void)
+{
+char hex
+for (hex = 48; hex <= 57; hex++)
+{
+putchar(putchar(hex);
+}
+for (hex = 97; hex <= 102; hex++)
+{
+putchar(hex);
+}
+putchar('\n');
+
+return (0);
+}
