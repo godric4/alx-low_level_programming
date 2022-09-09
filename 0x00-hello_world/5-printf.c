c475866c5bcecbb74cc4630599de1806804050ca
godric4 <gmarto400@gmail.com>
2022-09-08 17:10:22 -0700

    print me

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point of my code
+ * this code is needed on grammarly
+ * Return: 0 when function is successful
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,/n")
+return (0)
+}
