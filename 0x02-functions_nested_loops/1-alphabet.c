8bf31006ad634fd2708ee5d9a14ca1315f4ca649
godric4 <gmarto400@gmail.com>
2022-09-13 13:07:00 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void print_alphabet(void)
+{
+char i =  'a';
+char j = 'z';
+
+while (i <= j)
+{
+_putchar(i);
+i++;
+}
+_putchar('\n);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef main.h
+#define main.h
+
+/*the entire header file file */
+void print_alphabet(void)
+#endif
