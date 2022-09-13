9b4d7bfcafca7eb5bd09ef68e1e9f4c3867ac6d0
godric4 <gmarto400@gmail.com>
2022-09-13 16:49:28 -0700

    repeat 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - program entry point
+ * Return: 0 always successful
+ */
+
+void print_alphabet_x10(void)
+{
+char i;
+int j;
+for (j = 0; j < 10; j++)
+(
+ for (i = 'a'; i <= 'z'; i++;
+_putchar(i)
+_putchar('\n');
+}
+}
