e91394a55e34964be3eb9239cb4697a5390896ee
godric4 <gmarto400@gmail.com>
2022-09-16 14:55:20 -0700

    uppercase or not

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,11 @@
 #include "main.h"
 
 /**
- * _isupper - function to check alpah casing
- * Return: 1 if c is uppercase and 0 otherwise
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
  */
-
-int _isupper(int c)
+Int _isupper(int c)
 {
-int c;
-
-if(c >= 'A' && c <= 'Z')
-{ return (1)
-else 
-{ 
-return (0)
-}
-}
+return (c >= 'A' && c <= 'Z');
 }
