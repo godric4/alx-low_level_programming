ca30000a0d948213ea082a6b7f7861730e4732ff
godric4 <gmarto400@gmail.com>
2022-09-30 13:07:31 -0700

    fixed syntax error

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * main - prints the name of the program
  * @argc: argument count
  * @argv: argunent vector which is an array of strings
- * Return: 0 is success 
+ * Return: 0 is success
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
