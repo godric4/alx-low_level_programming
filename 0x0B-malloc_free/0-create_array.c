913a32f5e21c3214f5163fcb7d496ee4e8dec047
godric4 <gmarto400@gmail.com>
2022-10-03 14:01:34 -0700

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars
+ * @size: size of array
+ * @c: specific char value that initializes array of char
+ * Return: pointer to the array or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+char *mem_alloc;
+unsigned int i;
+
+mem_alloc = malloc(size * sizeof(char));
+for (i = 0; i < size; i++)
+mem_alloc[i] = c;
+mem_alloc[i] = 0;
+return (size < 1 ? 0 : mem_alloc);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,4 @@
+This repo contains different tasks 
+on how to dynamically allocate memory in C
+using the library function malloc
+1. 
