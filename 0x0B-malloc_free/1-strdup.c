020b8480548f1f9e0374f42cc4b0e19f3c46969e
godric4 <gmarto400@gmail.com>
2022-10-03 23:24:05 -0700

    Fixed bugs

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,11 +19,12 @@ if (str == NULL)
 return (NULL);
 
 for (i = 0; str[i] != '\0'; i++)
-new_mem_alloc = ((i + 1) * (char *)malloc(sizeof(char));
-	if (new_mem_alloc == NULL)
+new_mem_alloc = (char *)malloc(sizeof(char)* (i +1));
+	
+if (new_mem_alloc == NULL)
 return (NULL);
  
-for (j = 0; str[j] !== '\0'; j++)
+for (j = 0; str[j] <= 1; j++)
 {
 new_mem_alloc[j] = str [j]
 }
