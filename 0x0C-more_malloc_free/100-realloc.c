aa576de71d9c225deb28dc3b53a7354416a9d395
godric4 <gmarto400@gmail.com>
2022-10-06 21:01:15 -0700

    fixed bugs

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,7 +11,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *ptr1;
-	char *ptr2; //old pointer
+	char *ptr2;
 	unsigned int i;
 
 	if (new_size == old_size)
@@ -30,18 +30,18 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (!ptr1)
 		return (NULL);
 
-	ptr2  //old pointer= ptr;
+	ptr2
 
 	if (new_size < old_size)
 	{
 		for (i = 0; i < new_size; i++)
-			ptr1[i] = ptr2[ //old pointeri];
+			ptr1[i] = ptr2
 	}
 
 	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
-			ptr1[i] = ptr2[ //old pointeri];
+			ptr1[i] = ptr2
 	}
 
 	free(ptr);
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -4,7 +4,7 @@
 /**
  * *array_range - creates an array of integers
  * @min: minimum range of values stored
- * @max: maximum range of values stored 
+ * @max: maximum range of values stored
  * Return: pointer to the new array
  */
 int *array_range(int min, int max)
