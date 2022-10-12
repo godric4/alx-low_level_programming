4612bb5212a536bb265fecf5975a6b2989799618
godric4 <gmarto400@gmail.com>
2022-10-12 13:15:33 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - function to print name
+ * @name: pointer to name
+ * @f: print function pointer
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	f(name);
+}
