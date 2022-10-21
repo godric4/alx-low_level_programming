fb1eead3da61e6e8ef6600340e0f0673fa5c368b
godric4 <gmarto400@gmail.com>
2022-10-20 20:29:17 -0700

    project

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
-#include <stdio.h> 
-  
- /** 
-  * first - function executed before main 
-  * Return: no return. 
-  */ 
-  
- void __attribute__ ((constructor)) first() 
- { 
-         printf("You're beat! and yet, you must allow"); 
-         printf(",\nI bore my house upon my back!\n"); 
- }
+#include <stdio.h>
+
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - prints a sentence before the main
+ * function is executed
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,19 +1,19 @@
-#include "lists.h" 
-  
- /** 
-  * free_list - frees a list 
-  * @head: head of the linked list. 
-  * Return: no return. 
-  */ 
-  
- void free_list(list_t *head) 
- { 
-         list_t *current_node; 
-  
-         while ((current_node = head) != NULL) 
-         { 
-                 head = head->next; 
-                 free(current_node->str); 
-                 free(current_node); 
-         } 
- }
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
